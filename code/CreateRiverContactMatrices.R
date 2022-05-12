ea541d2b619f6e509a6e83da3611df179a9d7281
Sarah <sarah.alewijnse@cefas.co.uk>
2022-05-12 15:53:38 +0100

    Updated file paths so they work within project

--- code/CreateRiverContactMatrices.R
+++ code/CreateRiverContactMatrices.R
@@ -1,13 +1,13 @@
 #load("PreparedObjects.RData")
-source('SimulationCode2/CreateRiverContactMatrix.R')
+source('code/aquanet_functions/CreateRiverContactMatrix.R')
 
-riverDownstream.fileName = paste0("CalcRiverDistance/",Species,"/RoutesDownstreamTable.csv")
+riverDownstream.fileName = paste0("data/CalcRiverDistance/",Species,"/RoutesDownstreamTable.csv")
 graph.riverDownstream.objects = CreateRiverDistanceMatrix(riverDownstream.fileName, graph.contactp.objects, ListModelSetupParameters, 1)
 
 TestRiverDistanceMatrix(graph.riverDownstream.objects)
 graph.riverDistance.objects = list(graph.riverDownstream.objects, NULL)
 
 
-source('SimulationCode2/EstimateSite2SiteDistinances.R')
+source('code/aquanet_functions/EstimateSite2SiteDistinances.R')
 fomite.objects = CreateDistanceMatrix(graph_full, siteLocationsWithCatchmentDuplicatesRemoved.fileName, ListModelSetupParameters)
 list.fomite.objects = list(fomite.objects)
