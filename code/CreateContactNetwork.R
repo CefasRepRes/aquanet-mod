15d0b673f75bc09112780abc5df9e1e1fc2176bf
Sarah <sarah.alewijnse@cefas.co.uk>
2022-05-12 15:53:38 +0100

    Updated file paths so they work within project

--- code/CreateContactNetwork.R
+++ code/CreateContactNetwork.R
@@ -14,14 +14,13 @@ library(sp)
 library(rgeos)
 
 # Location of the Section 30 Records
-S30LFM.fileName = paste0('./SQL Files/',Species,'/S30LFM11-14.csv')
+S30LFM.fileName = paste0('data/SQL Files/',Species,'/S30LFM11-14.csv')
 
 # Location of the Farm to Farm Records
-Farm2FarmLFM.fileName = paste0('./SQL Files/',Species,'/Farm-farmLFM11-14.csv')
+Farm2FarmLFM.fileName = paste0('data/SQL Files/',Species,'/Farm-farmLFM11-14.csv')
 
 # Location of the EA Catchments
-catchmentLayer.fileName = "EA Catchments/catchmnt_50k+TrunkCodes-Filtered-Merged_region.shp"
-catchmentLayer.fileName = paste(LocationGISRootDirectory,catchmentLayer.fileName,sep="")
+catchmentLayer.fileName = "data/EA_Catchments/catchmnt_50k+TrunkCodes-Filtered-Merged_region.shp"
 
 # Location to save the site locations, together with catchment details
 siteLocationsWithCatchment.fileName = 'ListSiteLocationsWithCatchment.csv'
@@ -30,11 +29,11 @@ siteLocationsWithCatchment.fileName = 'ListSiteLocationsWithCatchment.csv'
 siteLocationsWithoutCatchment.fileName = 'ListSiteLocationsWithoutCatchment.csv'
 
 # Location of sites, following removal of duplicate sites
-siteLocationsWithCatchmentDuplicatesRemoved.fileName = 'ListSiteLocationsWithCatchmentNoDuplicates.csv'
+siteLocationsWithCatchmentDuplicatesRemoved.fileName = 'data/ListSiteLocationsWithCatchmentNoDuplicates.csv'
 
 # Location to save the contact network
 contactNetwork.fileName = 'combinedMovements.graph2.xml'
 
-source('SimulationCode2/CheckCatchmentSiteRelationships.R')
-source('SimulationCode2/importSiteData.R')
-source('SimulationCode2/graphSiteLevelStats.R')
+source('code/aquanet_functions/CheckCatchmentSiteRelationships.R')
+source('code/importSiteData.R')
+source('code/graphSiteLevelStats.R')
