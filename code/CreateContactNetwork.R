f4c26ed5d1ee757ad37fd5c883335c3de98d8f9d
Sarah <sarah.alewijnse@cefas.co.uk>
2022-05-17 14:40:58 +0100

    Remove rgeos package Don't run CheckCatchmentSiteRelationships as we have no duplicates file Updated name of contact network graph

--- code/CreateContactNetwork.R
+++ code/CreateContactNetwork.R
@@ -1,14 +1,11 @@
 #### Create contact network ####
 
-
 # Load required packges ---------------------------------------------------
 
 library(igraph) # Package for creating the contact network
-library(sf)
-library(rgeos) # Package for checking spatial relationships
+library(sf) # Package for working with spatial data
 library(here) # Package for getting filenames
 library(dplyr) # Package for manipulating tables
-              # TODO: see if this can be removed
 
 # Get file locations ------------------------------------------------------
 
@@ -53,11 +50,11 @@ contact_network_filename <- here::here("outputs",
 
 # Run sub-scripts ---------------------------------------------------------
 
-source('code/CheckCatchmentSiteRelationships.R') # Don't need to run this if you have no duplicates file already
+#source('code/CheckCatchmentSiteRelationships.R') # Don't need to run this if you have no duplicates file already
 source('code/importSiteData.R')
 
 # Save completed network 
 
-write.graph(combinedMovements.graph2, 
+write.graph(combined_movements_simplified, 
             file = contact_network_filename, 
             format = "graphml")
