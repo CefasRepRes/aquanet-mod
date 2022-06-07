ad7d2b0f14a738eac8de26e98baf4d6a9e4199e1
Sarah <sarah.alewijnse@cefas.co.uk>
2022-06-07 16:27:50 +0100

    Pulled river-based transmission parameters into parameter file

--- code/CreateRiverContactMatrices.R
+++ code/CreateRiverContactMatrices.R
@@ -2,14 +2,12 @@
 
 # Get downstream transmission probability matrix -------------------------------
 
-
-
 river_downstream_transmission_matrix <- 
   aquanet::createRiverDistanceProbabilityMatrix(
     filepath_river_distances = river_downstream_filename,
     out_createContactProbabilityMatrix = contact_probability_matrix,
-    max_dist = 40,
-    p1km = 0.0005)
+    max_dist = parameter_file$Max_Distance_River_Transmission,
+    p1km = parameter_file$Probability_River_Transmission)
 
 
 river_downstream_transmission_objects <- list(river_downstream_transmission_matrix, 
