045bc94892d3ac0706e05fea0bc27e590d19c8f9
Sarah <sarah.alewijnse@cefas.co.uk>
2022-06-07 17:14:56 +0100

    Updated README and numbered script files to better enable running/debugging

---
+++ code/06_RunCoreSimulationLoop-Parallel.R
@@ -0,0 +1,68 @@
+#### Run core simulation loop parallel ####
+
+# Setup -------------------------------------------------------------------
+
+# Clear the startup screen
+cat("\014")
+
+# Display without scientific notation
+options(scipen = 999)
+
+# Load functions ----------------------------------------------------------
+
+# TODO: replace with Becca's package
+
+source(here::here("code",
+                  "loop_functions",
+                  "simulation_code.R"))
+source(here::here("code",
+                  "loop_functions",
+                  "run_simulations.R"))
+
+# Run model ---------------------------------------------------------------
+
+print(
+  system.time( # Print the system time used by the runSimulations function
+    expr = {
+      runSimulations( 
+        graph.contactp.objects = contact_probability_matrix, 
+        simulationCode = simulationCode, 
+        ListRunTimeParameters = run_time_parameters_list, 
+        graph.withinCatchmentEdges.objects = within_catchment_movements, 
+        graph.catchment2Site.objects = catchment_site_matrix, 
+        graph.riverDistance.objects = river_downstream_transmission_objects, 
+        graph.estimateSiteDistances.objects = site_distances_matrix, 
+        farm_vector = farm_vector, 
+        noCores = noCores,
+        associatedSiteControlType = catchment_movement_controls,
+        locationSaveResults = save_results_filepath,
+        seedNo = seedNo,
+        initialNoInfections = initial_no_infections)
+    }
+  )
+)
+
+# # # Testing ----------------------------------------------------------------------
+# # 
+# # # # Set things to their original names
+# # 
+# graph.contactp.objects <- contact_probability_matrix
+# 
+# 
+# # Run model, without any parallel code, which is useful for debugging
+# set.seed(123)
+# 
+# system.time(expr =
+#               {simulationCode(graph.contactp.objects = contact_probability_matrix,
+#                               runs = 4,
+#                               tmax = tmax,
+#                               batchNo = 1,
+#                               ListRunTimeParameters = run_time_parameters_list,
+#                               graph.withinCatchmentEdges.objects = within_catchment_movements,
+#                               graph.catchment2Site.objects = catchment_site_matrix,
+#                               graph.riverDistance.objects = river_downstream_transmission_objects,
+#                               graph.estimateSiteDistances.objects = site_distances_matrix,
+#                               farm_vector = farm_vector,
+#                               associatedSiteControlType = 0,
+#                               locationSaveResults = save_results_filepath,
+#                               initialNoInfections = 1)})
