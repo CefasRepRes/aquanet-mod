42e828d1ea16fe38baacb3bc2c3cea9df6df10e6
Sarah <sarah.alewijnse@cefas.co.uk>
2022-07-18 17:19:40 +0100

    Got costs by sim for more accurate averages

--- code/Economics.R
+++ code/Economics.R
@@ -9,9 +9,12 @@ source(here::here("functions",
                   "load.results.site.type.R"))
 
 sites_summary_type <- load.results.site.type("full_run_for_economics")
+max(sites_summary_type$simNo)
 
 # Filter out non-farms
-sites_summary_type <- filter(sites_summary_type, farm_vector == 1)
+  # We don't have any data for fisheries
+sites_summary_farms <- filter(sites_summary_type, farm_vector == 1)
+max(sites_summary_farms$simNo)
 
 # Load in economic costing -----------------------------------------------------
 
@@ -23,46 +26,51 @@ daily_cost <- read.csv(here::here("data",
                                   "Economics",
                                   "daily_costs.csv"))
 
-# Filter by state --------------------------------------------------------------
-  # TODO: Expand to other states
+# Calculate daily costs --------------------------------------------------------
 
-source(here::here("analyses",
-                  "code",
-                  "fun_state_costs.R"))
+# Load function
+source(here::here("functions",
+                  "state.costs.R"))
 
+# Get a list of site types
 site_types <- cull_cost$site_type
 
-
-## Fallow ======================================================================
-
-fallow_costs <- state.costs(data = sites_summary_type,
+# Fallow
+fallow_costs <- state.costs(data = sites_summary_farms,
                             state = "fallow",
                             state_codes = c(4, 14, 24, 34),
                             site_types = site_types)
 
-## No management ===============================================================
+# No management
   # TODO: find out if this should only exist in the no controls scenario
 no_manage_costs <- state.costs(data = sites_summary_type,
                                state = "no_manage",
                                state_codes = 10,
                                site_types = site_types)
 
-## Contact tracing =============================================================
-
+# Contact tracing
 contact_trace_cost <- state.costs(data = sites_summary_type,
                                   state = "contact_trace",
                                   state_codes = c(1, 11, 21, 31,
                                                  7, 17, 27, 37),
                                   site_types = site_types)
 
-## Catchment controls ==========================================================
-
+# Catchment controls
 catchment_controls <- state.costs(data = sites_summary_type,
                                   state = "catchment_control",
-                                  state_codes = c(20:29),
+                                  state_codes = c(21, 22, 23, 24,
+                                                  25, 26, 27, 28, 29),
                                   site_types = site_types)
 
+# Combine into single daily cost data frame
+simulation_daily_costs <- fallow_costs %>% 
+  dplyr::full_join(no_manage_costs, by = "simNo") %>% 
+  dplyr::full_join(contact_trace_cost, by = "simNo") %>%
+  dplyr::full_join(catchment_controls, by = "simNo")
+
 # Add in cull costs ------------------------------------------------------------
+
+# TODO: finish testing
 
 fallow <- dplyr::filter(sites_summary_type, state %in% c(4, 14, 24, 34))
 sims <- unique(sites_summary_type$simNo)
@@ -79,6 +87,8 @@ for(i in 1:length(site_types)){
                                cull_cost_fhi = fhi_cull_cost)
   full_cull_cost <- rbind(full_cull_cost, comb_cull_cost)
 }}
+
+# TODO: combine and tidy
 
 # Make into data frame
 cull_costs <- data.frame(stage = c("cull_farm", "cull_fhi"),
