634fc454038241bb2243ff90932e0e3f7b4a7048
Sarah <sarah.alewijnse@cefas.co.uk>
2022-07-20 14:56:45 +0100

    Got it to record just the max

--- functions/time.per.stage.R
+++ functions/time.per.stage.R
@@ -9,24 +9,29 @@ sites_summary_type <- load(here::here("outputs",
                                       paste0(scenario_name, "-economics.Rdata")))
 
 sims <- unique(sites_summary_type$simNo)
-
+sims_site_time_summary <- data.frame()
 for(k in 1:length(sims)){
-  sim <- dplyr::filter(sites_summary_type, simNo == k)
-}
-
-test <- dplyr::filter(sites_summary_type, simNo == 1)
-test_siteID <- unique(sites_summary_type$siteID)
-test_site <- as.data.frame(dplyr::filter(test, siteID == 1191))
-
-trial <- test_site
-for(i in 1:nrow(test_site)){
-  if((test_site$state[i] == data.table::shift(test_site$state,
-                                              fill = 100)[i]) == T){
-    test_site$tdiff[i] <- test_site$tdiff[i] + test_site$tdiff[i - 1]
-  } else {
-    test_site$tdiff[i] <- test_site$tdiff[i]
+  sim <- dplyr::filter(sites_summary_type, simNo == sims[k])
+  site_ID <- unique(sim$siteID)
+  store <- data.frame()
+  for(s in 1:length(site_ID)){
+    by_site <- as.data.frame(dplyr::filter(sim, siteID == site_ID[s]))
+    by_site$recordID <- rownames(by_site)
+    remove_row <- rep(NA, nrow(by_site))
+    for(i in 1:nrow(by_site)){
+      if((by_site$state[i] == data.table::shift(by_site$state)[i]) %in% TRUE){
+        by_site$tdiff[i] <- by_site$tdiff[i] + by_site$tdiff[i - 1]
+        remove_row[[i]] <- as.numeric(i - 1)
+      } else {
+        by_site$tdiff[i] <- by_site$tdiff[i]
+      }
+    }
+    remove_row <- na.omit(remove_row)
+    by_site <- by_site %>% dplyr::filter(!(recordID %in% remove_row))
+    store <- rbind(store, by_site)
   }
+  sims_site_time_summary <- rbind(sims_site_time_summary, store)
 }
 
-test_site_2 <- test_site %>% group_by(state) %>% filter(tdiff == max(tdiff))
+
 library(dplyr)
