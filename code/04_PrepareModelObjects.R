d6dfe42ef89138f859561fea3ac188f7be2a6198
Sarah <sarah.alewijnse@cefas.co.uk>
2022-08-08 10:35:21 +0100

    Moved filepath definitions to 01_SetFilePaths.R

--- code/04_PrepareModelObjects.R
+++ code/04_PrepareModelObjects.R
@@ -16,6 +16,7 @@ objects_to_keep <- c("gis_filepath",
                      "parameterIndex", 
                      "save_results_filepath",
                      "site_locs_duplicates_removed_filename",
+                     "site_details_with_model_id_filename",
                      "scenario_name",
                      "data_collection_period",
                      "contact_network_filename",
@@ -41,26 +42,16 @@ graph_full <- read.graph(file = contact_network_filename, format = "graphml")
 # Including enough information to make it possible to infect specific sites within the model
 
 site_details_with_model_id <- aquanet::mergeGraphMetaWithCatchmentLocation(graph = graph_full, 
-                                                                           filename_sites_catchments = site_locs_duplicates_removed_filename) 
+                                                                           filename_sites_catchments = site_locs_duplicates_removed_filename)
 
-file_path_sites_with_model_id <- here::here("outputs",
-                                            paste0(scenario_name, "/"))
 write.csv(site_details_with_model_id,
-          file = here::here("outputs",
-                            scenario_name,
-                            "site_details_with_model_id.csv"), 
+          file = site_details_with_model_id_filename, 
           row.names = FALSE)
 
 # Get site - catchment relationships -------------------------------------------
 
-# Get file patch of catchment shapefile
-file_path_catchments <- here::here("data",
-                                   "EA_Catchments",
-                                   "catchmnt_50k+TrunkCodes-Filtered-Merged_region.shp")
-
-# Get catchment to site matrix
 catchment_site_matrix <- aquanet::createCatchmentToSiteMatrix(graph = graph_full,
-                                                              filename_catchment_layer = file_path_catchments,
+                                                              filename_catchment_layer = catchment_layer_filename,
                                                               crs_epsg = BNG_crs)
 
 # Extract within-catchment movements -------------------------------------------
