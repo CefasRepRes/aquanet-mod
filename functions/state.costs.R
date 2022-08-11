f816c37ae43cbddc302fd4ff325b463b4072e429
Sarah <sarah.alewijnse@cefas.co.uk>
2022-08-11 15:51:46 +0100

    Converted dplyr to data.table to speed up functions

--- functions/state.costs.R
+++ functions/state.costs.R
@@ -1,24 +1,27 @@
 state.costs <- function(data, state, state_codes, site_types){
+  system.time({
   # Filter data by state
-  selected_state <- dplyr::filter(data, state %in% state_codes)
-  state_summary <- selected_state
+    state_summary <- data[state %in% state_codes]
   # Get daily cost for that stage
-  state_daily_cost <- dplyr::filter(daily_cost, stage == state)
-  # Calculate duration costs for each site and sim
-  sims <- unique(state_summary$simNo)
-  costs <- data.frame()
+    daily_cost <- data.table(daily_cost)
+  state_daily_cost <- daily_cost[stage == state]
+    # Calculate duration costs for each site and sim
+  sims <- unique(state_summary[, simNo])
+  costs <- data.table()
+  length(costs) <- length(sims)
   for(k in 1:length(sims)){ # Loop over simNo
     for(i in 1:length(site_types)){ # Loop over site type
       type <- site_types[[i]] # Set site type
-      daily_site_cost <- dplyr::filter(state_daily_cost, site_type == type) # Select the cost for that site type
-      state_summary_by_sim <- dplyr::filter(state_summary, simNo == k) # Select the simNo
+      daily_site_cost <- state_daily_cost[site_type == type]
+      state_summary_by_sim <- state_summary[simNo == k]
       # Calculate the cost over the duration of the simulation
         # duration_cost = site type (1) * duration of time in the state * daily cost
-      duration_cost <- (state_summary_by_sim[, type] * state_summary_by_sim[, "tdiff"]) * daily_site_cost$farm_cost_per_day
+      duration_cost <- (state_summary_by_sim[, ..type] * state_summary_by_sim[, "tdiff"]) * daily_site_cost$farm_cost_per_day
       total_duration_cost <- as.data.frame(sum(duration_cost)) # Calculate the total cost over the duration
       total_duration_cost$simNo <- sims[k] # Add simulation number
       costs <- rbind(costs, total_duration_cost)
-    }}
+    }
+    print(sims[k])}
   
   # Make into data frame
   state_costs <- cbind(site_types, costs)
@@ -29,6 +32,7 @@ state.costs <- function(data, state, state_codes, site_types){
                                     values_from = duration_cost) # Reformat
   sim_summary$total_cost <- rowSums(sim_summary[, -1], na.rm = T) # Total cost [, -1] so as not to include the simNo in total
   colnames(sim_summary)[16] <- paste0(state, "_total_cost")
-  sim_summary <- dplyr::select(sim_summary, simNo, paste0(state, "_total_cost"))
+  sim_summary <- sim_summary[, c("simNo", paste0(state, "_total_cost"))]
   return(sim_summary)
+  })
 }
