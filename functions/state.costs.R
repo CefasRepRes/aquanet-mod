0ac6b7f4fe154191cabad7691fcc4fadbd5b064a
Sarah <sarah.alewijnse@cefas.co.uk>
2022-08-25 13:41:43 +0100

    Updated file names

--- functions/state.costs.R
+++ functions/state.costs.R
@@ -23,21 +23,21 @@ state.costs <- function(data, state, site_types){
     daily_cost <- data.table(daily_cost)
     state_daily_cost <- daily_cost[stage == state]
     # Calculate duration costs for each site and sim
-    sims <- unique(state_summary[, simNo])
+    sims <- unique(state_summary[, sim_no])
     costs <- data.table()
     length(costs) <- length(sims)
-    for(k in 1:length(sims)){ # Loop over simNo
+    for(k in 1:length(sims)){ # Loop over sim_no
       for(i in 1:length(site_types)){ # Loop over site type
         if(!(state %in% c("fallow", "no_manage", "contact_trace", "catchment_control"))) +
           stop("You have entered an incorrect state type. Please choose from fallow, no_manage, contact_trace or catchment_controls")
         type <- site_types[[i]] # Set site type
         daily_site_cost <- state_daily_cost[site_type == type]
-        state_summary_by_sim <- state_summary[simNo == k]
+        state_summary_by_sim <- state_summary[sim_no == k]
         # Calculate the cost over the duration of the simulation
         # duration_cost = site type (1) * duration of time in the state * daily cost
-        duration_cost <- (state_summary_by_sim[, ..type] * state_summary_by_sim[, "tdiff"]) * daily_site_cost$farm_cost_per_day
+        duration_cost <- (state_summary_by_sim[, ..type] * state_summary_by_sim[, "t_total"]) * daily_site_cost$farm_cost_per_day
         total_duration_cost <- as.data.frame(sum(duration_cost)) # Calculate the total cost over the duration
-        total_duration_cost$simNo <- sims[k] # Add simulation number
+        total_duration_cost$sim_no <- sims[k] # Add simulation number
         costs <- rbind(costs, total_duration_cost)
       }
       print(sims[k])}
@@ -49,8 +49,8 @@ state.costs <- function(data, state, site_types){
     # Calculate total cost over the course of a simulation
     sim_summary <- tidyr::pivot_wider(state_costs, names_from = site_types,
                                       values_from = duration_cost) # Reformat
-    sim_summary$total_cost <- rowSums(sim_summary[, -1], na.rm = T) # Total cost [, -1] so as not to include the simNo in total
+    sim_summary$total_cost <- rowSums(sim_summary[, -1], na.rm = T) # Total cost [, -1] so as not to include the sim_no in total
     colnames(sim_summary)[16] <- paste0(state, "_total_cost")
-    sim_summary <- sim_summary[, c("simNo", paste0(state, "_total_cost"))]
+    sim_summary <- sim_summary[, c("sim_no", paste0(state, "_total_cost"))]
     return(sim_summary)
 }
